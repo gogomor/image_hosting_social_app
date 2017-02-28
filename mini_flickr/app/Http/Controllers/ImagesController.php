ae45df8861a71d63ea609510d84e77dea8ea2586
Goran <gdjukic@live.com>
2017-03-01 00:31:16 +0100

    comments

--- mini_flickr/app/Http/Controllers/ImagesController.php
+++ mini_flickr/app/Http/Controllers/ImagesController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Image;
+use App\Comment;
 
 class ImagesController extends Controller
 {
     public function show(Image $image){
+    	$image->load('comments.user');
     	return view('image',compact('image'));
     }
 }
--- mini_flickr/resources/views/image.blade.php
+++ mini_flickr/resources/views/image.blade.php
@@ -5,5 +5,16 @@
 <img class="single-image" src="{{ asset('images/'. $image->path) }}"> 
 </div>
 
+<div class="comments">
+	@foreach($image->comments as $comment)
+		<div class="panel panel-info">
+		  <div class="panel-heading">{{ $comment->user->name }}</div>
+		  <div class="panel-body">
+		    {{ $comment->text }}
+		  </div>
+		</div>
+	@endforeach
+</div>
+
 
 @endsection
